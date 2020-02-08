62ab98d44f44ebcd73aaa416b10ffd183a237422
Ilyas <i.abdirahman@gmail.com>
2020-02-08 19:47:20 +0000

    Create a whole new context for the Alert component. AlertContext, 1 folder and 3 files. tidy up App.js to remove all props. Search.js use const alertContext = useContext(AlertContext)

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,16 +1,17 @@
 import React, {useState, useContext} from 'react'
-import PropTypes from 'prop-types'
+import AlertContext from '../../context/alert/alertContext'
 import GithubContext from '../../context/github/githubContext'
 
-const Search = ({setAlert}) => {
+const Search = () => {
 	const githubContext = useContext(GithubContext)
+	const alertContext = useContext(AlertContext)
 
 	const [text, setText] = useState('')
 
 	const onSubmit = (e) => {
 		e.preventDefault()
 		if (text === '') {
-			setAlert('Please enter something', 'light')
+			alertContext.setAlert('Please enter something', 'light')
 		} else {
 			githubContext.searchUsers(text)
 			setText('')
@@ -35,7 +36,6 @@ const Search = ({setAlert}) => {
 					className='btn btn-dark btn-block'
 				/>
 			</form>
-
 			{githubContext.users.length > 0 && (
 				<button
 					className='btn btn-light btn-block'
@@ -46,10 +46,6 @@ const Search = ({setAlert}) => {
 			)}
 		</div>
 	)
-}
-
-Search.propTypes = {
-	setAlert: PropTypes.func.isRequired,
 }
 
 export default Search
